a9a2567b744d27636ddf5cea7fd636f731c40a91
Mustakim <sycospike1@gmail.com>
2023-05-02 19:23:38 +0600

    genarte details data

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+const chefs = require('./data/chefs.json');
+
+app.use(cors());
 
 app.get('/', (req,res) => {
     res.send('Good Food is running')
 });
+
+app.get('/chefs', (req,res) =>{
+    res.send(chefs);
+})
 
 app.listen(port, () => {
     console.log(`Good Food is running on port: ${port}`)
