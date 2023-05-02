f3ad79860c4de21060ec1c050683e2bb36e361dd
Mustakim <sycospike1@gmail.com>
2023-05-02 19:44:26 +0600

    ready details server

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const chefs = require('./data/chefs.json');
+const details = require('./data/details.json');
 
 app.use(cors());
 
@@ -13,6 +14,17 @@ app.get('/', (req,res) => {
 
 app.get('/chefs', (req,res) =>{
     res.send(chefs);
+})
+
+app.get('/details', (req, res) => {
+    res.send(details);
+} );
+
+app.get('/details/:id', (req,res) => {
+    const id = parseInt(req.params.id) ;
+    console.log(id);
+    const selectedDetails = details.find(d => parseInt(d.id) === id);
+    res.send(selectedDetails)
 })
 
 app.listen(port, () => {
